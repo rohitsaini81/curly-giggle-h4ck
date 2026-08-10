fb53d0eaf8244f565ffdd814d66b76e686f75cd0
rohit saini <rohit1322saini@gmail.com>
2026-08-10 10:28:58 +0530

    hydrant error

--- instagram/insta/app/layout.tsx
+++ instagram/insta/app/layout.tsx
@@ -9,7 +9,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className="h-full antialiased">
-      <body className="min-h-full">{children}</body>
+      <body className="min-h-full" suppressHydrationWarning>
+        {children}
+      </body>
     </html>
   );
 }
